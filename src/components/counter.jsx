fcd9223fd2bde8f433de7a341d698fb60fb4bb27
Randy Phan <randyphan@Randys-MacBook-Pro.local>
2019-12-07 17:14:47 -0600

    more refactoring by making App.js the main component and adding a navbar to keep track of counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,7 +9,7 @@ class Counter extends Component {
           onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
-          Increment
+          +
         </button>
         <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
